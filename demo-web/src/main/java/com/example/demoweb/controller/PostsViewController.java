5d9a3cd887f855cc17a30fe6cb85dc8a132e211e
Sekou Samassa <sksamassa@gmail.com>
2022-09-17 19:26:56 +0500

    updated class PostViewController

--- demo-web/src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ demo-web/src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,7 @@
 package com.example.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostsViewController {
     @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Сентябрь горит");
+        return "list";
     }
 
     @ResponseBody
